c3006e6997c62369751fe34748ce5b7156458d61
Trevi Awater <Trevi@TreviModdingCrew.nl>
2013-05-29 22:56:40 +0200

    Removed Voidcraft
    Version 0.0.0.1
    
    - Moving Files

--- Voidcraft/TreviModdingCrew/Voidcraft/Proxy/ClientProxy.java
+++
@@ -1,6 +0,0 @@
-package TreviModdingCrew.Voidcraft.Proxy;
-
-public class ClientProxy extends CommonProxy
-{
-
-}
--- Voidcraft/TreviModdingCrew/Voidcraft/Proxy/CommonProxy.java
+++
@@ -1,6 +0,0 @@
-package TreviModdingCrew.Voidcraft.Proxy;
-
-public class CommonProxy
-{
-
-}
