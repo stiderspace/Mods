0ca5208eb9e6b7efdc9c6ea1721c4738b3fb35f4
Trevi Awater <Trevi@TreviModdingCrew.nl>
2013-05-31 15:48:49 +0200

    Fixed First Person Render
    Version 0.1.1.1
    
    - Darker Blades
    - Fixed First Person Render

--- Minecraft Utilities/TreviModdingCrew/Utilities/Renders/RenderLumberJacker.java
+++ Minecraft Utilities/TreviModdingCrew/Utilities/Renders/RenderLumberJacker.java
@@ -37,19 +37,25 @@ public class RenderLumberJacker implements IItemRenderer
         {
             case ENTITY:
             {
-                Render(0f, 0f, 0f, 0.5f);
+                Render(0f, 0f, 0f, 0.5f, ItemRenderType);
                 return;
             }
             
             case EQUIPPED:
             {
-                Render(0.5F, 0F, 0.5F, 0.5F);
+                Render(0.5F, 0F, 0.5F, 0.5F, ItemRenderType);
+                return;
+            }
+            
+            case EQUIPPED_FIRST_PERSON: 
+            {
+                Render(0.5F, 0.0F, 0.5F, 0.5F, ItemRenderType);
                 return;
             }
              
             case INVENTORY:
             {
-                Render(0f, -0.5f, 0f, 0.5f);
+                Render(0f, -0.5f, 0f, 0.5f, ItemRenderType);
                 return;
             }
             
@@ -57,7 +63,7 @@ public class RenderLumberJacker implements IItemRenderer
         }
     }
     
-    private void Render(float Var1, float Var2, float Var3, float Scale)
+    private void Render(float Var1, float Var2, float Var3, float Scale, ItemRenderType ItemRenderType)
     { 
         GL11.glPushMatrix();
         GL11.glDisable(GL11.GL_LIGHTING);
@@ -65,6 +71,16 @@ public class RenderLumberJacker implements IItemRenderer
         GL11.glTranslatef(Var1,  Var2,  Var3);
         GL11.glScalef(Scale, Scale, Scale);
         GL11.glRotatef(0F, 0F, 1F, 0F);
+        
+        if(ItemRenderType == ItemRenderType.EQUIPPED_FIRST_PERSON)
+        {
+            GL11.glRotatef(-90F, 0F, 1F, 0F);
+        }
+        
+        if(ItemRenderType == ItemRenderType.EQUIPPED)
+        {
+            GL11.glRotatef(180F, 0F, 1F, 0F);
+        }
         
         FMLClientHandler.instance().getClient().renderEngine.bindTexture("/TreviModdingCrew/Utilities/Textures/Model/Lumber Jacker.png");
         
